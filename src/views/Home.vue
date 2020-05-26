bc6d1aa8c467c20c197aa40ee295ecc92c5e8d94
liuweihua <liuweihua>
2020-05-26 21:36:24 +0800

    完善tab

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -23,16 +23,24 @@
     <van-tabs v-model="activeIndex" animated sticky swipeable @change="tabChange">
       <van-tab :title="item.name" :name="index" v-for="(item,index) in tabList" :key="item.name">
         <van-list
-          v-model="loading[activeIndex]"
+          :immediate-check="false"
           :finished="finished[activeIndex]"
           finished-text="没有更多了"
           @load="getRecomData"
         >
-          <div v-for="(item,index) in tabData[activeIndex]" :key="item.code+index">{{item.title}}</div>
+          <div class="dish-wrap">
+            <div
+              class="dish-item"
+              v-for="(item,index) in tabData[activeIndex]"
+              :key="item.code+index"
+            >
+              <img :src="item.image.url">
+              <div class="bottom">
+                <div class="title">{{item.title}}</div>
+              </div>
+            </div>
+          </div>
         </van-list>
-        <!-- <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="getRecomData">
-          <van-cell v-for="item in list" :key="item" :title="item"/>
-        </van-list>-->
       </van-tab>
     </van-tabs>
   </div>
@@ -62,7 +70,7 @@ export default {
         this.modelList = res;
       });
     },
-    // tab基础数据及首屏数据
+    // tab基础数据及第一屏数据
     getTabData() {
       request("/baidu/v1/index/home?")
         .then(res => {
@@ -76,35 +84,44 @@ export default {
             }
           });
           this.tabData[this.activeIndex] = res.first.list;
-          this.nextUrls[this.activeIndex] = res.first.nextUrl;
-        });
-    },
-    // 请求推荐数据
-    getRecomData() {
-      // if(!this.nextUrls[this.activeIndex]) {
-      //   this.loading[this.activeIndex] = false;
-      //   return;
-      // }
-      request("/baidu/v1/index/recom?" + this.nextUrls[this.activeIndex]||'').then(
-        res => {
-          this.tabData[this.activeIndex] = [
-            ...(this.tabData[this.activeIndex] || []),
-            ...res.list
-          ];
           this.$set(
             this.tabData,
             this.activeIndex,
             this.tabData[this.activeIndex]
           );
-          this.nextUrls[this.activeIndex] = res.nextUrl;
-        }
-      );
+          this.nextUrls[this.activeIndex] = res.first.nextUrl;
+        });
+    },
+    // 请求推荐数据
+    getRecomData() {
+      console.log(this.loading);
+
+      if (this.loading[this.activeIndex]) return;
+      this.loading[this.activeIndex] = true;
+      request(
+        "/baidu/v1/index/recom?" + this.nextUrls[this.activeIndex] || ""
+      ).then(res => {
+        this.tabData[this.activeIndex] = [
+          ...(this.tabData[this.activeIndex] || []),
+          ...res.list
+        ];
+        this.$set(
+          this.tabData,
+          this.activeIndex,
+          this.tabData[this.activeIndex]
+        );
+        this.nextUrls[this.activeIndex] = res.nextUrl;
+        this.loading[this.activeIndex] = false;
+      });
     },
     // tab切换
     tabChange(name) {
       this.activeIndex = name;
       this.nextUrls[this.activeIndex] =
         "type=" + this.tabList[this.activeIndex].type;
+      if (!this.tabData[this.activeIndex]) {
+        this.getRecomData();
+      }
     }
   },
   created() {
@@ -116,6 +133,7 @@ export default {
 
 <style lang="less">
 .home {
+  background-color: #fff;
   .top {
     border-bottom: 0.1rem solid #f3f3f3;
   }
@@ -182,6 +200,36 @@ export default {
     .van-tabs__line {
       width: 47px;
       transform: translateX(47px) translateX(-50%);
+    }
+  }
+}
+.dish-wrap {
+  display: flex;
+  box-sizing: border-box;
+  width: 100%;
+  padding: 0 15px;
+  flex-direction: row;
+  justify-content: space-between;
+  flex-wrap: wrap;
+
+  .dish-item {
+    border-radius: 0.1rem;
+    box-shadow: 0 0 0.1rem #e6e6e6;
+    width: 3.34rem;
+    overflow: hidden;
+    margin: 10px 0;
+  }
+  .bottom {
+    padding: 0.1rem 0.15rem 0.2rem;
+    .title {
+      font-size: 0.26rem;
+      line-height: 0.36rem;
+      font-weight: bold;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      display: -webkit-box;
+      -webkit-line-clamp: 2;
+      -webkit-box-orient: vertical;
     }
   }
 }
